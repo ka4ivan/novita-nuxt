eb159e24cd421c9e2d58930d6d67521a57ae86bd
Kachula Ivan <ka4ivan.dev@gmail.com>
2025-11-23 12:46:07 +0200

    error page

---
+++ src/error.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <BaseHeader />
+
+  <div class="container error__container">
+    <h1 class="error__title">404</h1>
+
+    <div class="error__subtitle">Сторінка не знайдена</div>
+
+    <div class="error__text">Можливо, вона була видалена, або ви ввели неправильну адресу</div>
+
+    <NuxtLink to="/" class="error__link">
+      Перейти на головну
+    </NuxtLink>
+  </div>
+
+  <BaseFooter />
+</template>  
